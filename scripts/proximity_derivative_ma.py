e62d40df8c3d03384f5b574b3b624d20c6d937b6
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2023-12-16 16:33:12 +0900

    save data as csv and use moving average

---
+++ scripts/proximity_derivative_ma.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import rospy
+from pr2_fingertip_sensors.msg import SensorArray
+import numpy as np
+
+WINDOW_SIZE = 4
+
+class ProximityDerivativeMA(object):
+    def __init__(self):
+        self.gripper = 'r_gripper'
+        self.fingertips = ['l_fingertip', 'r_fingertip']
+        self.queue = {}
+        self.pubs = {}
+        self.subs = {}
+        for fingertip in self.fingertips:
+            self.queue[fingertip] = np.zeros((WINDOW_SIZE, 4))
+            self.subs[fingertip] = rospy.Subscriber(
+                '/pfs/{}/{}/proximity_derivative'.format(self.gripper, fingertip),
+                SensorArray, self.cb, fingertip, queue_size=1)
+            self.pubs[fingertip] = rospy.Publisher(
+                '/pfs/{}/{}/proximity_derivative_ma'.format(self.gripper, fingertip),
+                SensorArray, queue_size=1)
+
+    def cb(self, msg, fingertip):
+        self.queue[fingertip] = np.append(self.queue[fingertip], np.array(msg.data).reshape(1, 4), axis=0)
+        self.queue[fingertip] = np.delete(self.queue[fingertip], 0, axis=0)
+        pub_msg = SensorArray(header=msg.header)
+        pub_msg.data = np.average(self.queue[fingertip], axis=0)
+        self.pubs[fingertip].publish(pub_msg)
+
+if __name__ == '__main__':
+    rospy.init_node('proximity_derivative_ma')
+    pd = ProximityDerivativeMA()
+    rospy.spin()
--- scripts/save_data_with_force_trigger.py
+++ scripts/save_data_with_force_trigger.py
@@ -14,12 +14,23 @@ class SaveDatawithForceTrigger(object):
         self.gripper = 'r_gripper'
         self.fingertips = ['l_fingertip', 'r_fingertip']
         self.pfs_params = rospy.get_param('/pfs')
+
+        self.filepath = {}
+        savedir = rospy.get_param('~savedir', '/home/nakane/Documents')
+        dt = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+        indices = ['time', 'label'] + ['prox_{}'.format(i) for i in range(4)]
+
         self.rate = rospy.Rate(100)
         self.count = {}
         self.prox_buffer = {}
         self.sub = {}
         self.touch_status = {}
         for fingertip in self.fingertips:
+            self.filepath[fingertip] = '{}/{}_{}_{}.csv'.format(savedir, dt, self.gripper, fingertip)
+            with open(self.filepath[fingertip], 'a') as f:
+                writer = csv.writer(f)
+                writer.writerow(indices)
+
             self.count[fingertip] = {'T':0, 'R':0, '0':0}
             self.prox_buffer[fingertip] = []
             self.touch_status[fingertip] = None
@@ -28,7 +39,7 @@ class SaveDatawithForceTrigger(object):
                 '/boolean_node_checking_conditions_{}/output/or'.format(fingertip),
                 Bool, self.bool_cb, fingertip, queue_size=1)
             self.sub[fingertip]['proximity'] = rospy.Subscriber(
-                '/pfs/{}/{}/proximity_derivative'.format(self.gripper, fingertip),
+                '/pfs/{}/{}/proximity_derivative_ma'.format(self.gripper, fingertip),
                 SensorArray, self.prox_cb, fingertip, queue_size=10)
 
     def bool_cb(self, msg, fingertip):
@@ -48,6 +59,7 @@ class SaveDatawithForceTrigger(object):
             else:
                 rospy.loginfo("{} 'T':{} 'R':{} '0':{}".format(fingertip, self.count[fingertip]['T'], self.count[fingertip]['R'], self.count[fingertip]['0']))
         self.touch_status[fingertip] = msg.data
+        rospy.sleep(0.5)
 
     def prox_cb(self, msg, fingertip):
         self.prox_buffer[fingertip].append(msg)
@@ -58,7 +70,7 @@ class SaveDatawithForceTrigger(object):
         idx = 0
         for i, p in enumerate(self.prox_buffer[fingertip]):
             self.rate.sleep()
-            if (detect_time - p.header.stamp).to_sec() > 1.0:
+            if (detect_time - p.header.stamp).to_sec() > 2.0:
                 idx = i
             elif (detect_time - p.header.stamp).to_sec() > 0.5:
                 # discard old data
@@ -84,7 +96,7 @@ class SaveDatawithForceTrigger(object):
                 if (p.header.stamp - detect_time).to_sec() < -0.05:
                     # discard old data
                     pass
-                elif (p.header.stamp - detect_time).to_sec() < 0.3:
+                elif (p.header.stamp - detect_time).to_sec() < 0.4:
                     self.save(p, 'R', fingertip)
                 else:
                     self.prox_buffer[fingertip] = self.prox_buffer[fingertip][i:]
@@ -97,7 +109,7 @@ class SaveDatawithForceTrigger(object):
             # wait for new data
             self.rate.sleep()
             now = rospy.Time.now()
-        time.sleep(0.2) # stop saving
+        time.sleep(0.5) # stop saving
 
     def get_free_data(self, prox_data, fingertip):
         # Max10個保存
@@ -105,9 +117,13 @@ class SaveDatawithForceTrigger(object):
         for p in random_data:
             self.save(p, '0', fingertip)
 
-    def save(self, proximity, label, fingertip):
+    def save(self, p, label, fingertip):
         self.count[fingertip][label] += 1
-                
+        save_data = [p.header.stamp, label] + list(p.data)
+        with open(self.filepath[fingertip], 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(save_data)
+
 if __name__ == '__main__':
     rospy.init_node('save_data_with_force_trigger')
     sd = SaveDatawithForceTrigger()
